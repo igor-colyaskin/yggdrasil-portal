644c81585b4c5aeec817d45559596a3a0bad61c6
Igor Colyaskin <igor.colyaskin@gmail.com>
2026-01-20 19:35:58 +0400

    move lib to right place, fix issue with element's hight

--- app/staffportal/webapp/helpers/CardManager.js
+++ app/staffportal/webapp/helpers/CardManager.js
@@ -2,27 +2,53 @@ sap.ui.define([
     "sap/ui/base/Object",
     "sap/ui/core/Fragment"
 ], function (BaseObject, Fragment) {
-    "use strict";
+    "use strict"
 
     return BaseObject.extend("com.epic.yggdrasil.staffportal.helpers.CardManager", {
         constructor: function (oController) {
-            this._oController = oController;
-            this._oView = oController.getView();
+            this._oController = oController
+            this._oView = oController.getView()
         },
 
-        generate: function (aCards) {
-            aCards.forEach(oCard => {
-                const sZoneId = oCard.target + "Zone";
-                const oContainer = this._oView.byId(sZoneId);
+        // generate: function (aCards) {
+        //     aCards.forEach(oCard => {
+        //         const sZoneId = oCard.target + "Zone";
+        //         const oContainer = this._oView.byId(sZoneId);
 
+        //         Fragment.load({
+        //             id: this._oView.getId() + "--" + oCard.id,
+        //             name: "com.epic.yggdrasil.staffportal.cards." + oCard.fragment,
+        //             controller: this._oController
+        //         }).then(oFragment => {
+        //             oContainer.addContent(oFragment);
+        //         });
+        //     });
+        // }
+        generate: function (aCards) {
+            const oView = this._oView // Сохраняем ссылку на вьюху
+            const oController = this._oController
+
+            aCards.forEach(oCard => {
+                // Запускаем загрузку фрагмента
                 Fragment.load({
-                    id: this._oView.getId() + "--" + oCard.id,
-                    name: "com.epic.yggdrasil.staffportal.view.fragments." + oCard.fragment,
-                    controller: this._oController
-                }).then(oFragment => {
-                    oContainer.addContent(oFragment);
-                });
-            });
+                    id: oView.getId() + "--" + oCard.id,
+                    name: "com.epic.yggdrasil.staffportal.cards." + oCard.fragment,
+                    controller: oController
+                }).then(function (oFragment) {
+                    // Ищем контейнер ТОЛЬКО когда фрагмент уже готов
+                    const sZoneId = oCard.target + "Zone"
+                    const oContainer = oView.byId(sZoneId)
+
+                    if (oContainer && typeof oContainer.addItem === "function") {
+                        oContainer.addItem(oFragment)
+                        console.log("✅ Голем [" + oCard.id + "] успешно приземлился в " + sZoneId)
+                    } else {
+                        console.error("❌ Не удалось найти VBox с ID: " + sZoneId + " или у него нет метода addContent")
+                    }
+                }).catch(function (oError) {
+                    console.error("💀 Ошибка призыва Голема " + oCard.id + ":", oError)
+                })
+            })
         }
-    });
-});
+    })
+})
