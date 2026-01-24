69f8d81f91535455834be322ca5507a52518f655
Igor Colyaskin <igor.colyaskin@gmail.com>
2026-01-24 15:02:32 +0400

    put card settings to json in model folder

--- app/staffportal/webapp/Component.js
+++ app/staffportal/webapp/Component.js
@@ -5,23 +5,23 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "com/epic/yggdrasil/staffportal/lib/sdkcard/StorageUtils"
 ], function (UIComponent, Host, EventProvider, JSONModel, StorageUtils) {
-    "use strict";
+    "use strict"
 
     return UIComponent.extend("com.epic.yggdrasil.staffportal.Component", {
         metadata: { manifest: "json" },
 
         init: function () {
             // 1. Инициализируем хранилище сразу
-            StorageUtils.createStorage("YGG_PORTAL_2026");
+            StorageUtils.createStorage("YGG_PORTAL_2026")
 
             // 2. Инициализируем UI-модель (состояние)
-            this._initUIModel();
+            this._initUIModel()
 
             // 3. Базовая инициализация UIComponent
-            UIComponent.prototype.init.apply(this, arguments);
+            UIComponent.prototype.init.apply(this, arguments)
 
             // 4. Настройка Хоста (инфраструктуры)
-            this._setupHost();
+            this._setupHost()
         },
 
         /**
@@ -29,65 +29,59 @@ sap.ui.define([
          * Данные из этой модели будут пробрасываться в карточки.
          */
         _initUIModel: function () {
-            const oInitialState = {
-                selectedEmployeeID: StorageUtils.readItem("selectedID") || "",
-                currentTab: "staff",
-                userRole: "Wizard",
-                // Конфигурация карточек (можно вынести в отдельный JSON файл при желании)
-                cards: [
-                    { id: "headerCard", containerId: "headerSection", manifest: "./cards/HeaderCard/manifest.json" },
-                    { id: "navCard", containerId: "navSection", manifest: "./cards/NavCard/manifest.json" },
-                    { id: "staffTableCard", containerId: "staffTableContainer", manifest: "./cards/StaffTable/manifest.json" },
-                    { id: "contextStripCard", containerId: "contextStripContainer", manifest: "./cards/ContextStrip/manifest.json" }
-                ]
-            };
+            // 1. Создаем модель и сразу грузим данные из JSON
+            const oModel = new JSONModel()
+            oModel.loadData(sap.ui.require.toUrl("com/epic/yggdrasil/staffportal/model/CardConfig.json"), null, false)
+            // false для синхронной загрузки, чтобы карточки были доступны сразу
 
-            const oModel = new JSONModel(oInitialState);
-            this.setModel(oModel, "ui");
+            // 2. Дополняем модель данными из Storage (которые нельзя хранить в статичном JSON)
+            oModel.setProperty("/selectedEmployeeID", StorageUtils.readItem("selectedID") || "")
+            oModel.setProperty("/currentTab", StorageUtils.readItem("currentTab") || "staff")
+
+            this.setModel(oModel, "ui")
         },
-
         /**
          * Настройка "epicHost", "Эфирного Резонантора" и Context Bridge
          */
         _setupHost: function () {
-            this._oHost = new Host("epicHost");
-            this._oResonator = new EventProvider();
-            const oUiModel = this.getModel("ui");
+            this._oHost = new Host("epicHost")
+            this._oResonator = new EventProvider()
+            const oUiModel = this.getModel("ui")
 
             // --- 1. Shared Context Management (через UI модель) ---
             // Теперь контекст Хоста всегда возвращает актуальные данные из нашей модели "ui"
             this._oHost.getContext = () => {
-                return Promise.resolve(oUiModel.getData());
-            };
-            
+                return Promise.resolve(oUiModel.getData())
+            }
+
             this._oHost.setContext = (mCtx) => {
                 if (mCtx) {
                     // Обновляем модель (это автоматически обновит биндинги во всех карточках)
                     Object.keys(mCtx).forEach(sKey => {
-                        console.log(`📡 [Component]: Updating model key "${sKey}" with value:`, mCtx[sKey]); // <-- ПРОВЕРКА 2
-                        oUiModel.setProperty("/" + sKey, mCtx[sKey]);
-                    });
-                    
+                        console.log(`📡 [Component]: Updating model key "${sKey}" with value:`, mCtx[sKey]) // <-- ПРОВЕРКА 2
+                        oUiModel.setProperty("/" + sKey, mCtx[sKey])
+                    })
+
                     // Если изменился ID — сохраняем в Storage
                     if (mCtx.selectedEmployeeID) {
-                        StorageUtils.setItem("selectedID", mCtx.selectedEmployeeID);
+                        StorageUtils.setItem("selectedID", mCtx.selectedEmployeeID)
                     }
 
                     // Уведомляем карточки об изменении конфигурации
-                    this._oHost.fireEvent("configurationChange");
-                    console.log("🌐 [Host Context]: Updated & Persisted", oUiModel.getData());
+                    this._oHost.fireEvent("configurationChange")
+                    console.log("🌐 [Host Context]: Updated & Persisted", oUiModel.getData())
                 }
-            };
+            }
 
             // --- 2. Эфирный Резонантор (PubSub) ---
             this._oHost.publishEvent = (sName, oData) => {
-                this._oResonator.fireEvent(sName, oData);
-                console.log(`📡 [Resonator]: Published -> ${sName}`, oData);
-            };
+                this._oResonator.fireEvent(sName, oData)
+                console.log(`📡 [Resonator]: Published -> ${sName}`, oData)
+            }
 
             this._oHost.subscribeEvent = (sName, fnCallback, oListener) => {
-                this._oResonator.attachEvent(sName, fnCallback, oListener);
-            };
+                this._oResonator.attachEvent(sName, fnCallback, oListener)
+            }
 
             // --- 3. Destination Resolver ---
             // Централизованное управление адресами сервисов
@@ -96,16 +90,16 @@ sap.ui.define([
                     "hrService": "/odata/v4/hr",
                     "financeService": "/finance",
                     "projectService": "/odata/v4/projects"
-                };
-                return mDestinations[sName] || ""; 
-            };
+                }
+                return mDestinations[sName] || ""
+            }
         },
 
         /**
          * Публичный доступ к Хосту для контроллеров и карточек
          */
-        getHost: function () { 
-            return this._oHost; 
+        getHost: function () {
+            return this._oHost
         }
-    });
-});
+    })
+})
